d24b409c01926f70514ad69ba3e19ee5be63c9b7
Viswajeet Lenka <viswajeetlenka9@gmail.com>
2025-04-11 15:08:42 +0000

    Added cors middleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
 # from app.database import engine, Base
 from app.auth import auth_router
 from app.routes import router
 
 app = FastAPI()
+
+# origins = [
+#     "http://localhost:4200",  # Angular local development
+#     #"https://yourfrontend.com",  # Production frontend URL
+# ]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,  # Allows specific origins
+#     allow_credentials=True,
+#     allow_methods=["*"],  # Allows all HTTP methods (GET, POST, PUT, DELETE, etc.)
+#     allow_headers=["*"],  # Allows all headers
+# )
+
 app.include_router(auth_router)
 app.include_router(router)
 
